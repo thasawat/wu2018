3d985c648d686d3415b8598afeee2ece61c3b85a
thasawat <rthasawat@gmaill.com>
2018-02-04 09:50:04 +0700

    Edit Test

--- tests/AiTest.php
+++ tests/AiTest.php
@@ -10,4 +10,23 @@ final class AITest extends TestCase
         $expected_result = 'Male';
         $this->assertEquals($expected_result, $result);
     }
+
+    public function testGender_Female(): void
+    {
+        $result = AI::getGender('สวัสดีค่ะ');
+        $expected_result = 'Female';
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGender_Unknown(): void
+    {
+        $result = AI::getGender('');
+        $expected_result = 'Unknown';
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testSentiment_Positive(): void
+    {
+        $result = AI::getPositive('น่ารัก');
+        $expected_result = 'Positive';
+        $this->assertEquals($expected_result, $result);
+    }
 }
